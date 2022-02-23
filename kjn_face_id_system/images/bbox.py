e4486506f9ff4148aa49b4d42fd481ee01f59987
kornellewy <40360922+kornellewy@users.noreply.github.com>
2022-02-23 06:16:00 +0100

    add get coco to bbox

--- kjn_face_id_system/images/bbox.py
+++ kjn_face_id_system/images/bbox.py
@@ -27,3 +27,15 @@ class BBox:
     def preproces_line(self, line: list) -> dict:
         line = [round(float(num), 2) for num in line]
         return line[1], line[2], line[3], line[4], line[5]
+
+    def get_coco(self):
+        x_top_left = self.x_center - self.width / 2
+        y_top_left = self.y_center - self.height / 2
+        return {
+            "bbox_class": self.bbox_class,
+            "x_top_left": x_top_left,
+            "y_top_left": y_top_left,
+            "width": self.width,
+            "height": self.height,
+            "confidence": self.confidence,
+        }
